f2225adaa3a669c76c1a10e11418229b0b00d9ba
Nerz <adrian.dufac@gmail.com>
2024-12-10 13:46:29 +0100

    creation of first cube ina scene

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 
+const canvas = document.querySelector("canvas.webgl");
+
 //Scene
 const scene = new THREE.Scene();
 
@@ -10,4 +12,25 @@ const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
 
+//sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
 //Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+
+canvas.width = sizes.width;
+canvas.height = sizes.height;
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
