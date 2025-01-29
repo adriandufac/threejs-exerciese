8a27f2d5b40c841fb89f27aa73e751ab83991a6c
Nerz <adrian.dufac@gmail.com>
2025-01-29 11:12:36 +0100

    addition of fog

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -413,6 +413,11 @@ sky.material.uniforms["rayleigh"].value = 3;
 sky.material.uniforms["mieCoefficient"].value = 0.1;
 sky.material.uniforms["mieDirectionalG"].value = 0.95;
 sky.material.uniforms["sunPosition"].value.set(0.3, -0.038, -0.95);
+
+/*Fog */
+//scene.fog = new THREE.Fog("#02343f", 1, 15);
+scene.fog = new THREE.FogExp2("#02343f", 0.1);
+
 /**
  * Animate
  */
