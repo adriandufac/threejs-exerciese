41bed0e253cfc3ff4534c4575a14586e0eee00d6
Nerz <adrian.dufac@gmail.com>
2025-02-03 10:43:36 +0100

    addition of colors according to partticle position

--- 18-galaxy-generator/src/script.js
+++ 18-galaxy-generator/src/script.js
@@ -25,6 +25,9 @@ parameters.radius = 5;
 parameters.branches = 3;
 parameters.spin = 1;
 parameters.randomness = 0.2;
+parameters.randomnessPower = 3;
+parameters.insideColor = "#ff6030";
+parameters.outsideColor = "#1b3984";
 
 let particlesGeometry = null;
 let particlesMaterial = null;
@@ -39,35 +42,59 @@ const generateGalaxy = () => {
   }
 
   const positionsArray = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
+  const insideColor = new THREE.Color(parameters.insideColor);
+  const outsideColor = new THREE.Color(parameters.outsideColor);
+
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
 
     const radius = Math.random() * parameters.radius;
+    const colorAlpha = radius / parameters.radius;
+    const mixedColor = insideColor.clone().lerp(outsideColor, colorAlpha);
+
     const branchAngle =
       ((i % parameters.branches) / parameters.branches) * Math.PI * 2; // (/parameters.branches) to get a value between 0 and 1 and * Math.PI * 2 to get a value between 0 and 2PI
     // cos(branchAngle) = x/radius
     // sin(branchAngle) = y/radius
     const spinAngle = parameters.spin * radius;
-    const randomX = (Math.random() - 0.5) * parameters.randomness;
-    const randomY = (Math.random() - 0.5) * parameters.randomness;
-    const randomZ = (Math.random() - 0.5) * parameters.randomness;
+
+    colors[i3 + 0] = mixedColor.r;
+    colors[i3 + 1] = mixedColor.g;
+    colors[i3 + 2] = mixedColor.b;
+
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      parameters.randomness *
+      (Math.random() < 0.5 ? 1 : -1);
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      parameters.randomness *
+      (Math.random() < 0.5 ? 1 : -1);
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      parameters.randomness *
+      (Math.random() < 0.5 ? 1 : -1);
     positionsArray[i3 + 0] =
       radius * Math.cos(branchAngle + spinAngle) + randomX;
     positionsArray[i3 + 1] = randomY;
     positionsArray[i3 + 2] =
       radius * Math.sin(branchAngle + spinAngle) + randomZ;
   }
+
   particlesGeometry = new THREE.BufferGeometry();
   particlesGeometry.setAttribute(
     "position",
     new THREE.BufferAttribute(positionsArray, 3)
   );
+  particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
   particlesMaterial = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true, // to create perspective
     depthWrite: false,
     blending: THREE.AdditiveBlending,
+    vertexColors: true,
   });
 
   particles = new THREE.Points(particlesGeometry, particlesMaterial);
@@ -119,6 +146,16 @@ gui
   .max(2)
   .step(0.001)
   .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "randomnessPower")
+  .min(1)
+  .max(10)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+
+gui.addColor(parameters, "insideColor").onFinishChange(generateGalaxy);
+gui.addColor(parameters, "outsideColor").onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
