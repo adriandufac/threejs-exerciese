5dc8c532c460c13d7112742209f02935c5aec874
Nerz <adrian.dufac@gmail.com>
2025-02-04 08:12:31 +0100

    refacto : creating a function to create spheres

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -7,6 +7,15 @@ import CANNON from "cannon";
  * Debug
  */
 const gui = new GUI();
+const debugObject = {};
+debugObject.createSphere = () => {
+  createSphere(Math.random(), {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+gui.add(debugObject, "createSphere");
 
 /**
  * Base
@@ -64,7 +73,7 @@ world.addContactMaterial(concretePlasticContactMaterial);
 world.addContactMaterial(defaultContactMaterial);
 
 //Shpere
-const sphereShape = new CANNON.Sphere(0.5);
+/* const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
@@ -76,7 +85,7 @@ sphereBody.applyLocalForce(
   new CANNON.Vec3(150, 0, 0),
   new CANNON.Vec3(0, 0, 0)
 );
-world.addBody(sphereBody);
+world.addBody(sphereBody); */
 world.defaultContactMaterial = defaultContactMaterial;
 
 //floor
@@ -91,7 +100,7 @@ world.addBody(floorBody);
 /**
  * Test sphere
  */
-const sphere = new THREE.Mesh(
+/* const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 32, 32),
   new THREE.MeshStandardMaterial({
     metalness: 0.3,
@@ -102,7 +111,7 @@ const sphere = new THREE.Mesh(
 );
 sphere.castShadow = true;
 sphere.position.y = 0.5;
-scene.add(sphere);
+scene.add(sphere); */
 
 /**
  * Floor
@@ -188,6 +197,42 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+/*
+ * Utils
+ */
+
+const objectsToUpdate = [];
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+const createSphere = (radius, position) => {
+  // Three.js mesh
+
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  mesh.scale.set(radius, radius, radius);
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Sphere(radius);
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  objectsToUpdate.push({ mesh, body });
+};
+
+createSphere(0.5, { x: 0, y: 3, z: 0 });
+
 /**
  * Animate
  */
@@ -200,10 +245,14 @@ const tick = () => {
   oldElapsedTime = elapsedTime;
 
   // Update physics world
+  //sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position); // wind to the sphere
   world.step(1 / 60, deltaTime, 3);
 
+  objectsToUpdate.forEach((object) => {
+    object.mesh.position.copy(object.body.position);
+  });
   // Update sphere
-  sphere.position.copy(sphereBody.position);
+  // sphere.position.copy(sphereBody.position);
 
   // Update controls
   controls.update();
