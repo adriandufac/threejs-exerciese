ed1fa88233a8a1bdbf684f092b9cddc7f45fa500
Nerz <adrian.dufac@gmail.com>
2025-02-04 07:22:16 +0100

    addition of floor physic

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -48,6 +48,14 @@ const sphereBody = new CANNON.Body({
 
 world.addBody(sphereBody);
 
+//floor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI * 0.5);
+
+world.addBody(floorBody);
 /**
  * Test sphere
  */
