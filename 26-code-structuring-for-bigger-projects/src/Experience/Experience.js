3e66b396f3d318f642998dfea3508e16eee86eed
Nerz <adrian.dufac@gmail.com>
2025-02-13 14:37:34 +0100

    addition of utils class to handle the sizes and the clock

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,6 +1,30 @@
+import * as THREE from "three";
+import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time.js";
+
 export default class Experience {
   constructor(canvas) {
-    this.canvas = canvas;
     window.experience = this; // just to access it in the console
+
+    //options
+    this.canvas = canvas;
+
+    //setup
+    this.sizes = new Sizes();
+    this.time = new Time();
+    this.scene = new THREE.Scene();
+
+    // resize event
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+
+    // Time tick event
+    this.time.on("tick", () => {
+      this.update();
+    });
   }
+
+  resize() {}
+  update() {}
 }
---
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Time.js
@@ -0,0 +1,33 @@
+import EventEmitter from "./EventEmitter";
+
+export default class Time extends EventEmitter {
+  constructor() {
+    super();
+
+    //setup
+    this.start = Date.now();
+    this.current = this.start;
+    this.elapsed = 0;
+    this.delta = 16.666;
+
+    window.requestAnimationFrame(() => {
+      this.tick();
+    });
+  }
+
+  tick() {
+    console.log("tick");
+
+    const currentTime = Date.now();
+    this.delta = currentTime - this.current;
+    this.current = currentTime;
+    this.elapsed = this.current - this.start;
+    console.log(this.elapsed);
+
+    this.trigger("tick");
+
+    window.requestAnimationFrame(() => {
+      this.tick();
+    });
+  }
+}
