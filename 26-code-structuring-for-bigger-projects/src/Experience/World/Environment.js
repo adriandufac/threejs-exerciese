a590ac589be5f420f5c88c2e7971d3fb9e2bd33f
Nerz <adrian.dufac@gmail.com>
2025-02-17 08:38:56 +0100

    additiion of tweak for environment in gui

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
@@ -6,6 +6,10 @@ export default class Environment {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+    this.debug = this.experience.debug;
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder("environment");
+    }
 
     this.setSunLight();
     this.setEnvironmentMap();
@@ -21,6 +25,37 @@ export default class Environment {
     this.sunLight.position.set(3.5, 2, -1.25);
 
     this.scene.add(this.sunLight);
+
+    //debug
+    if (this.debug.active) {
+      this.debugFolder
+        .add(this.sunLight, "intensity")
+        .min(0)
+        .max(10)
+        .step(0.001)
+        .name("sunLightIntensity");
+
+      this.debugFolder
+        .add(this.sunLight.position, "x")
+        .min(-5)
+        .max(5)
+        .step(0.001)
+        .name("sunLightX");
+
+      this.debugFolder
+        .add(this.sunLight.position, "y")
+        .min(-5)
+        .max(5)
+        .step(0.001)
+        .name("sunLightY");
+
+      this.debugFolder
+        .add(this.sunLight.position, "z")
+        .min(-5)
+        .max(5)
+        .step(0.001)
+        .name("sunLightZ");
+    }
   }
 
   setEnvironmentMap() {
@@ -44,5 +79,16 @@ export default class Environment {
       });
     };
     this.environmentMap.updateMaterial();
+
+    //debug
+    if (this.debug.active) {
+      this.debugFolder
+        .add(this.environmentMap, "intensity")
+        .min(0)
+        .max(4)
+        .step(0.001)
+        .name("envMapIntensity")
+        .onChange(this.environmentMap.updateMaterial);
+    }
   }
 }
