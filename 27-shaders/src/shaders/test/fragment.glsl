39d3a9432856cd0f50a5dacafef52a4cd099b7bd
Nerz <adrian.dufac@gmail.com>
2025-02-18 11:28:08 +0100

    using a geometry attribute to use in vertex shader then a varying to pass the value to fragment shader

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
+
+varying float vRandom; // coming from vertex shader
   
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(0.6, vRandom, 1.0, 1.0);
 }
--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -2,14 +2,19 @@ uniform mat4 projectionMatrix; // transform into clip space coordinates
 uniform mat4 viewMatrix;  // about the camera
 uniform mat4 modelMatrix; // converted from mesh position, rotation, scale
 
+attribute float aRandom; // random value we created coming from geometry
 attribute vec3 position;// position coming from geometry
+
+varying float vRandom; // pass to fragment shader
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0 ) * 0.1;
-    
+    modelPosition.z += aRandom * 0.1;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
+
+    vRandom = aRandom;
 }
