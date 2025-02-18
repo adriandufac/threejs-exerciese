0c68610597d916bc7ce692c9cbcb2e63f4e7f791
Nerz <adrian.dufac@gmail.com>
2025-02-18 14:49:14 +0100

    using elevation to add some shadows

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -4,9 +4,10 @@ uniform vec3 uColor;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
-
+varying float vElevation;
   
 void main() {
     vec4 texture = texture2D(uTexture, vUv);
+    texture.rgb *= vElevation* 2.0 + 0.5;
     gl_FragColor = vec4(texture.r, texture.g, texture.b, texture.a);
 }
