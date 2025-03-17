5b60bcde6fcf226144e0c5c7f4b7731c69a6b0e0
Nerz <adrian.dufac@gmail.com>
2025-03-17 12:01:29 +0100

    addition of perling noise and small waves to have a more realistic render

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -21,11 +21,11 @@ const scene = new THREE.Scene();
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
 
 // Colors
 debugObject.depthColor = "#0000ff";
-debugObject.surfaceColor = "#8888ff";
+debugObject.surfaceColor = "#90d6f4";
 
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
@@ -37,6 +37,11 @@ const waterMaterial = new THREE.ShaderMaterial({
     uBigWavesElevation: { value: 0.2 },
     uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
     uBigWavesSpeed: { value: 0.75 },
+
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallWavesIterations: { value: 4.0 },
 
     uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
     uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
@@ -74,6 +79,30 @@ gui.addColor(debugObject, "depthColor").onChange(() => {
 gui.addColor(debugObject, "surfaceColor").onChange(() => {
   waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
 });
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSmallWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name("uSmallWavesFrequency");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uSmallWavesSpeed");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesIterations, "value")
+  .min(0)
+  .max(8)
+  .step(1)
+  .name("uSmallWavesIterations");
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial);
 water.rotation.x = -Math.PI * 0.5;
--- 29-raging-sea/src/shaders/water/fragment.glsl
+++ 29-raging-sea/src/shaders/water/fragment.glsl
@@ -6,7 +6,8 @@ varying float vElevation;
 
 
 void main (){
-    float elevationNormalized = (vElevation + uBigWavesElevation) / (2.0 * uBigWavesElevation);
+    // Calculer l'élévation maximale possible
+    float elevationNormalized = (vElevation + uBigWavesElevation) / (2.0 * max(uBigWavesElevation, 0.01));
     vec3 color = mix(uDepthColor, uSurfaceColor, elevationNormalized );
 
     gl_FragColor = vec4(color, 1.0);
