cca7bc57192bb0e524dc6ba3c1c73c892dc22090
Nerz <adrian.dufac@gmail.com>
2025-03-24 17:03:29 +0100

    impleminting circle shaped particules

--- 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,4 +1,8 @@
  void main() {
-      gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    
+    float strength = 1.0-step( 0.5,distance(gl_PointCoord, vec2(0.5)));
+
+
+      gl_FragColor = vec4(vec3(strength), 1.0);
       #include <colorspace_fragment>
     }
