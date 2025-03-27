fcbb4792fe06481bed46148da23b913e2bd131cb
Nerz <adrian.dufac@gmail.com>
2025-03-27 14:36:22 +0100

    fixing the normals

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -91,25 +91,46 @@ material.onBeforeCompile = (shader) => {
     `
   );
   shader.vertexShader = shader.vertexShader.replace(
-    "#include <begin_vertex>",
-    `#include <begin_vertex>
-    
+    "#include <beginnormal_vertex>",
+    `
+    #include <beginnormal_vertex>
     float angle = (position.y + uTime) * 0.5;
     mat2 rotateMatrix = get2dRotateMatrix(angle);
+    objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  );
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <begin_vertex>",
+    `#include <begin_vertex>
+
     transformed.xz = rotateMatrix * transformed.xz;
     `
   );
 };
 
 depthMaterial.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime;
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <common>",
+    `
+      #include <common>
+      uniform float uTime;
+      mat2 get2dRotateMatrix(float _angle)
+      {
+          return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+      }
+      `
+  );
+
   shader.vertexShader = shader.vertexShader.replace(
     "#include <begin_vertex>",
     `#include <begin_vertex>
-
-        float angle = (position.y + uTime) * 0.5;
-        mat2 rotateMatrix = get2dRotateMatrix(angle);
-        transformed.xz = rotateMatrix * transformed.xz;
-        `
+      
+      float angle = (position.y + uTime) * 0.5;
+      mat2 rotateMatrix = get2dRotateMatrix(angle);
+      transformed.xz = rotateMatrix * transformed.xz;
+      `
   );
 };
 
